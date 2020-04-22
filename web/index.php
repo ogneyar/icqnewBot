9372f04d58f8cbadf7d560927e9b884b85582f92
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-04-22 19:55:31 +0300

    Add files via upload

--- web/index.php
+++ web/index.php
@@ -1,3 +1,19 @@
 <?
-echo "ЭЭЭЭЭЭЭЭ уфь";
+// Подключаем библиотеку с классом Bot
+include_once 'myBotApi/Bot.php';
+
+$token = "001.2839288818.3919878723:752122979";
+
+// Создаем объект бота
+$bot = new Bot($token);
+
+$id_bota = substr(strstr($token, ':'), 1);	
+
+$события = $bot->getEvents(0,0);
+
+echo "<pre>"; print_r($события); echo "</pre>";
+
+//echo "ЭЭЭЭЭЭЭЭ уфь";
+
+
 ?>
