a5eefa761f78dbb4bb553526ee01e6f1d1377674
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-04-23 09:55:12 +0300

    Add files via upload

--- web/index.php
+++ web/index.php
@@ -9,32 +9,11 @@ $bot = new Bot($token);
 
 $id_bota = substr(strstr($token, ':'), 1);	
 
-$eventId = null;
+$eventId = 0;
+
+$bot->sendText("@Ogneyar_", "Цикл начат.");
 
 do {
-	if (!$eventId) {
-		$события = $bot->getEvents(0,0);
-
-		foreach($события as $event) {
-			$eventId = $event['eventId'];
-			$payload = $event['payload'];
-				$chat = $payload['chat'];
-					$chatId = $chat['chatId'];
-					$chatType = $chat['type'];
-				$from = $payload['from'];
-					$firstName = $from['firstName'];
-					$nick = $from['nick'];
-					$userId = $from['userId'];
-				$msgId = $payload['msgId'];
-				$text = $payload['text'];
-				$timestamp = $payload['timestamp'];
-			$type = $event['type'];
-		}
-	}//else ++$eventId;
-	echo "Последнее событие: ".$eventId."<br><br>";
-
-	//echo "<pre>"; print_r($события); echo "</pre>";
-
 	$события = $bot->getEvents($eventId,150);
 
 	foreach($события as $event) {
@@ -53,6 +32,10 @@ do {
 		$type = $event['type'];
 	}
 	
+	if (!$eventId) $eventId = 0;
+	
+	echo "Последнее событие: ".$eventId."<br><br>";
+	
 	if ($text=='q') {
 		$bot->sendText($chatId, "клмн");
 	}elseif ($text) $bot->sendText($chatId, "я не понимаю(");
@@ -61,5 +44,7 @@ do {
 
 echo "Цикл окончен. <br><br>";
 $bot->sendText("@Ogneyar_", "Цикл окончен.");
+
+echo "<pre>"; print_r($события); echo "</pre>";
 
 ?>
